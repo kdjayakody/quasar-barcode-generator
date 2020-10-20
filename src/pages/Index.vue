5923a0ff59ee2155a88643b604036197302cf85d
Kavidu Jayakody <kdjayakodypro@gmail.com>
2020-10-20 20:15:20 +0530

    Barcode Settings Updated, Image Download

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,56 @@
 <template>
-<div class="q-pa-lg">
-    <center class="q-mt-xl">
+<div class="q-pr-xl q-pl-xl">
+    <center class="q-mt-md">
         <h1>KDJ QR Generator</h1>
-        <q-input outlined v-model="barcodeValue" /><br>
-        <barcode :value="barcodeValue" textPosition="bottom" height="50" width="1" textMargin="5" lineColor="black" format="CODE39" displayValue=TRUE>
-            Enter Your Text and Happy QR
-            <br>Powered By Quasar Framework
-        </barcode>
+        <p class="text-left">Barcode Settings</p>
+
+        <div class="row q-col-gutter-xs">
+            <div class="col-4">
+                <q-input filled v-model="options.lineColor" class="my-input">
+                    <template v-slot:append>
+                        <q-icon name="colorize" class="cursor-pointer">
+                            <q-popup-proxy transition-show="scale" transition-hide="scale">
+                                <q-color v-model="options.lineColor" />
+                            </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                </q-input>
+            </div>
+            <div class="col-4">
+                <q-input filled v-model="options.background" class="my-input">
+                    <template v-slot:append>
+                        <q-icon name="colorize" class="cursor-pointer">
+                            <q-popup-proxy transition-show="scale" transition-hide="scale">
+                                <q-color v-model="options.background" />
+                            </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                </q-input>
+            </div>
+
+            <div class="col-1">
+                <q-input label="Font Size" v-model.number="options.fontSize" type="number" filled style="max-width: 200px" />
+            </div>
+            <div class="col-1">
+                <q-input label="Width" v-model.number="options.width" type="number" filled style="max-width: 200px" />
+            </div>
+            <div class="col-1">
+                <q-input label="Height" v-model.number="options.height" type="number" filled style="max-width: 200px" />
+            </div>
+            <div class="col-1">
+                <q-input label="Margin" v-model.number="options.margin" type="number" filled style="max-width: 200px" />
+            </div>
+        </div>
+        <div class="barcode-container q-pt-lg">
+            <q-input rounded outlined v-model="barcodeValue" placeholder="Item Code" /><br>
+            <p>Barcode Result</p>
+            <barcode :value="barcodeValue" :elementTag="tag" :background="options.background" :lineColor="options.lineColor" :fontSize="options.fontSize" :margin="options.margin" :width="options.width" :height="options.height">
+            </barcode>
+            <div v-if="barcodeValue">
+                <button @click="downloadBarcode($event)">Download Now</button>
+                <p class="q-pt-xs">If buttton didn't work, right click and save the image</p>
+            </div>
+        </div>
 
     </center>
 
@@ -15,18 +59,40 @@
 
 <script>
 import VueBarcode from 'vue-barcode';
+
 export default {
+
     data() {
         return {
-            barcodeValue: ''
+
+            barcodeValue: '',
+            tag: 'canvas',
+            options: {
+                lineColor: '#ff7069',
+                fontSize: 15,
+                font: 'Courier',
+                width: 1,
+                height: 60,
+                margin: 10,
+                format: 'MSI',
+                background: '#ffffff00'
+            },
         }
     },
     components: {
         'barcode': VueBarcode
+    },
+    methods: {
+        downloadBarcode(event) {
+            var canvas = document.getElementsByClassName("vue-barcode-element");
+            var image = canvas[0].toDataURL("image/png");
+            console.log(image)
+            event.href = image
+        }
     }
 }
 </script>
 
-<style>
+<style lang="css" scoped>
 
 </style>
